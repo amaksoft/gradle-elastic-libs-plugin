f2875cf897f93afed7c91983cbd894e5b4fd8b00
Andrey Makeev <andrey.makeev@altarix.ru>
2016-08-19 18:17:23 +0400

    Change Licence metadata for Bitray, add environment variable check to cancel local libs connections

--- src/main/groovy/com/github/amaksoft/ElasticLibs/SettingsUtils.groovy
+++ src/main/groovy/com/github/amaksoft/ElasticLibs/SettingsUtils.groovy
@@ -4,18 +4,25 @@ package com.github.amaksoft.ElasticLibs
  * Created by amak on 8/18/16.
  */
 class SettingsUtils {
+    String envVarSwitch = "JENKINS_URL" // because we use Jenkins ;)
 
     def isValidProjectDir(String pathToDir) {
         new File(pathToDir, "build.gradle").exists()
     }
 
+    boolean checkEnv() {
+        System.getenv(envVarSwitch) == null
+    }
+
     def includeIfValid(String projectName, String pathToDir) {
-        if (isValidProjectDir(pathToDir)) {
+        if (isValidProjectDir(pathToDir) && checkEnv()) {
             include projectName
             project(projectName).projectDir = new File(pathToDir)
             println "Including local project \"" + projectName + "\" (\"" + pathToDir + "\")"
-        } else {
+        } else if (!isValidProjectDir(pathToDir)){
             println "Not including local project \"" + projectName + "\" (\"" + pathToDir + "\"). Local project doesn't exist."
+        } else {
+            println "Not including local project \"" + projectName + "\" (\"" + pathToDir + "\"). Cancelled by with environment variable \"$envVarSwitch\""
         }
     }
 }
